cccd2d5cca30678dab6d3f651b15b4158147776f
decio2022 <113923924+decio2022@users.noreply.github.com>
2025-12-29 16:15:16 -0330

    Add versioning and changelog to mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,7 @@
 let modInfo = {
 	name: "Ultimate Button Simulator",
 	id: "btn_sim_01",
+	author: "decio2022",
 	pointsName: "Money",
 	modFiles: ["layers.js", "tree.js"],
 	discordName: "",
@@ -9,6 +10,22 @@ let modInfo = {
 	offlineLimit: 1, 
 }
 
+let VERSION = {
+	num: "1.0",
+	name: "Release",
+}
+
+let changelog = `<h1>Changelog:</h1><br>
+	<h3>v0.0</h3><br>
+		- Added things.<br>
+		- Added stuff.`
+
+let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+
+// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
+// (The ones here are examples, all official functions are already taken care of)
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
 }
@@ -16,6 +33,13 @@ function getStartPoints(){
 function canGenPoints(){
 	return true
 }
+
+function addedPlayerData() { return {
+}}
+
+// Display extra things at the top of the page
+var displayThings = [
+]
 
 function getPointGen() {
 	if (!canGenPoints()) return new Decimal(0)
